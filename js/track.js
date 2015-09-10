92e75e6befb99d9c8c8513af36c2698058ba6b8e
averted <vitiuk.alex@gmail.com>
2015-09-09 20:06:00 -0400

    Adding ga events tracking

---
+++ js/track.js
@@ -0,0 +1,17 @@
+(function() {
+  var nodes = [
+    { id: 'js-now',    name: 'Now' },
+    { id: 'js-past',   name: 'Past' },
+    { id: 'js-github', name: 'GitHub' },
+    { id: 'js-email',  name: 'Email' },
+    { id: 'js-travel', name: 'Travel' },
+  ];
+
+  for (var i = 0; i < nodes.length; i++) {
+    var node = document.getElementById(nodes[i].id);
+
+    node.addEventListener('click', function() {
+      ga("send", "event", "button", "click", nodes[i].name);
+    });
+  }
+})();
